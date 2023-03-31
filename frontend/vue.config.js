fdd2ab0e46dd4545eb46cbfcf40a5c89a8698adb
Roman Ismagilov <roman.j123@ya.ru>
2023-03-31 12:03:53 +0300

    reset / vue config file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  publicPath: process.env.NODE_ENV === 'production'
-    ? './var/www/ismrdvlpr_ru_usr34/data/www/ismrdvlpr.ru/frontend/dist'
-    : '/'
-
+  transpileDependencies: true
 })
