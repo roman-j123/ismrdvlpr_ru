34c11d00cc4027f06cb0e1f4bd1ec2968fa63d77
Roman Ismagilov <roman.j123@ya.ru>
2023-03-31 11:59:10 +0300

    fix / config vue file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/var/www/ismrdvlpr_ru_usr34/data/www/ismrdvlpr.ru/frontend/dist'
+    : '/'
+
 })
