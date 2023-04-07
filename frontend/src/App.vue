dde04e2d46eed11497b8ff2370348230023c3488
Roman Ismagilov <roman.j123@ya.ru>
2023-04-07 22:27:55 +0300

    add / axios module

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="IMSRDVLPR.RU VUE APP" />
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul class="post__list" v-else>
+      <li class="post__item" v-for="post in posts" :key="post.id">
+        <h2>{{ post.attributes.title }}</h2>
+        <p>{{ post.attributes.text }}</p>
+      </li>
+    </ul>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from 'axios';
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data () {
+    return {
+      posts: [],
+      error: null
+    }
   },
+  async mounted () {
+    try {
+      const response = await axios.get('http://ismrdvlpr.ru:1337/api/posts')
+      this.posts = response.data.data.sort((a, b) => b.id - a.id);
+    } catch (error) {
+      this.error = error;
+    }
+  }
 };
 </script>
 
@@ -19,8 +37,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.post__list {
+  margin: 0 auto;
+  width: 80%;
+  list-style: none;
 }
 </style>
